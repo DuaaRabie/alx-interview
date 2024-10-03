fdee3f40a6cb5794b87b1e2287abc0d8a9131102
Duaa Rabie <duaarabie11@gmail.com>
2024-10-03 20:12:27 +0000

    update code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,22 @@
 def pascal_triangle(n):
-	"""
-	Generate Pascal's triangle up to the nth row.
-	Args:
-		n (int): The number of rows in the triangle.
-    
-	Returns:
-	list: A list of lists representing Pascal's triangle.
-	"""
-	if n <= 0:
-		return []
-    
-	result = [[1]]
-	for i in range(1, n):
-		row = [1]
-        	prev_row = result[i-1]
-        	for j in range(1, i):
-            		row.append(prev_row[j-1] + prev_row[j])
-        	row.append(1)
-        	result.append(row)
-    
+    """
+    Generate Pascal's triangle up to the nth row.
+    Args:
+    n (int): The number of rows in the triangle.
+
+    Returns:
+    list: A list of lists representing Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+
+    result = [[1]]
+    for i in range(1, n):
+        row = [1]
+        prev_row = result[i-1]
+    for j in range(1, i):
+        row.append(prev_row[j-1] + prev_row[j])
+        row.append(1)
+        result.append(row)
+
     return result
