39930adddbb7796342c84b9798e782a11f87089c
Duaa Rabie <duaarabie11@gmail.com>
2024-10-10 11:47:50 +0000

    update code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,10 +8,8 @@ def canUnlockAll(boxes):
     for i, box in enumerate(boxes):
         if i not in unlocked_boxes:
             unlocked_boxes.add(i)
-            
             # Check all keys in the current box
             for key in box:
                 if key not in unlocked_boxes:
                     unlocked_boxes.add(key)
-    
     return len(unlocked_boxes) == len(boxes)
