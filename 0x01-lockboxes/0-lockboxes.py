2f3cccecb587befac4905f0136768988eab427dd
Duaa Rabie <duaarabie11@gmail.com>
2024-10-10 12:32:00 +0000

    update code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """ unlock boxes """
 
+
 def canUnlockAll(boxes):
     """ Unlock boxes"""
     unlocked = [False] * len(boxes)
     unlocked[0] = True
-    
     for i in range(len(boxes)):
         for key in boxes[i]:
             if 0 <= key < len(boxes) and i != key:
                 unlocked[key] = True
-    
     return all(unlocked)
