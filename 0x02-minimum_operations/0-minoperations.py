efa4142f87443c673d34cecd117ce817eb7ccf25
Duaa Rabie <DuaaRabie11@gmail.com>
2024-10-17 19:26:25 +0000

    update code

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Minimum Operations """
+
+
+def minOperations(n):
+    """ min Operations function """
+    if n == 0:
+        return 0
+
+    operations = 0  # Initial copy operation
+
+    # Loop through powers of 2 up to n
+    power_of_two = 2
+    while power_of_two <= n:
+        operations += 1  # Add paste operation
+        power_of_two *= 2
+
+    return operations + 1
