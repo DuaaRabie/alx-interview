25d6c3e1612aa5a021367c482d7f2fe0f18a5d99
Duaa Rabie <DuaaRabie11@gmail.com>
2024-10-17 19:55:40 +0000

    update code

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,10 +7,10 @@ def minOperations(n):
     if n <= 0:
         return 0
     
-    # Convert n to binary and remove the '0b' prefix
-    bin_n = bin(n)[2:]
-    
-    # Find the position of the leftmost '1'
-    max_power = len(bin_n) - 1 - bin_n.rfind('1')
+    # Find the position of the leftmost '1' bit
+    max_power = 0
+    while n:
+        n >>= 1
+        max_power += 1
     
     return max_power if max_power >= 0 else 0
