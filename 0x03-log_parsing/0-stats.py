9d959170da34a62370789ba62c2715ee8d425e92
Duaa Rabie <DuaaRabie11@gmail.com>
2024-10-25 02:25:53 +0000

    update code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -49,7 +49,6 @@ def main():
         for code in sorted_status_codes:
             if status_counts[code] > 0:
                 print(f"{code}: {status_counts[code]}")
-        sys.exit()
     finally:
         print(f"File size: {total_file_size}")
         sorted_status_codes = \
@@ -57,6 +56,7 @@ def main():
         for code in sorted_status_codes:
             if status_counts[code] > 0:
                 print(f"{code}: {status_counts[code]}")
+        sys.exit()
 
 
 if __name__ == "__main__":
