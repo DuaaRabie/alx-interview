f910a41ba05b48bb0b1fee2421c969990b36c37a
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-05 18:57:30 +0000

    update code

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,18 +10,18 @@ def island_perimeter(grid):
     for index, row in enumerate(grid):
         prev_element = 0
         for inner_index, element in enumerate(row):
-            if element:
+            if element == 1:
                 current_row_land.append(inner_index)
-                if not prev_element or inner_index == 0:
+                if prev_element == 0 or inner_index == 0:
                     perimeter += 1
                 if inner_index not in prev_row_land:
                     perimeter += 1
-                if not row[inner_index + 1] or inner_index == len(row) + 1:
+                if row[inner_index + 1] == 0 or inner_index == len(row) + 1:
                     perimeter += 1
-                prev_element = element
             else:
                 if inner_index in prev_row_land:
                     perimeter += 1
+            prev_element = element
         prev_row_land = current_row_land[:]
         current_row_land = []
     return perimeter
