6ab4e2c731b02666ab34fa020ff0a232b5a2e66f
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-05 18:49:23 +0000

    task0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """ returns the perimeter of the island described in grid"""
+    perimeter = 0
+    prev_row_land = []
+    current_row_land = []
+    for index, row in enumerate(grid):
+        prev_element = 0
+        for inner_index, element in enumerate(row):
+            if element:
+                current_row_land.append(inner_index)
+                if not prev_element or inner_index == 0:
+                    perimeter += 1
+                if inner_index not in prev_row_land:
+                    perimeter += 1
+                if not row[inner_index + 1] or inner_index == len(row) + 1:
+                    perimeter += 1
+                prev_element = element
+            else:
+                if inner_index in prev_row_land:
+                    perimeter += 1
+        prev_row_land = current_row_land[:]
+        current_row_land = []
+    return perimeter
